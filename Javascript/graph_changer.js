64f2b94de5be8362f4076a748e9a95513d23690a
TomMakesThings <61354833+TomMakesThings@users.noreply.github.com>
2021-08-05 22:40:09 +0100

    Added web application section

--- Javascript/graph_changer.js
+++ Javascript/graph_changer.js
@@ -1,70 +1,90 @@
-var label_graph_count = 0;
-var label_graph_id = ["movies-per-genre", "genres-per-movie"];
+// Counters to record which graph is currently on display
+var label_count = 0;
+var pie_count = 0;
+var desc_count = 0;
+var model_count = 0;
+var ldia_count = 0;
+var lsa_count = 0;
 
-var pie_graph_count = 0;
-var pie_graph_id = ["label-pie-raw", "label-pie-reduced", "label-pie-even"];
-
-var desc_graph_count = 0;
-var desc_graph_id = ["scattertext-horror", "scattertext-romance", "frequency-distribution", "bigrams", "trigrams"];
-
-var model_graph_count = 0;
-var model_graph_id = ["model-diagram", "model-matrix", "model-pie"];
+// Lists of IDs for the order of graphs to display
+var label_id = ["movies-per-genre", "genres-per-movie"];
+var pie_id = ["label-pie-raw", "label-pie-reduced", "label-pie-even"];
+var desc_id = ["scattertext-horror", "scattertext-romance", "frequency-distribution", "bigrams", "trigrams"];
+var model_id = ["model-diagram", "model-matrix", "model-pie"];
+var ldia_id = ["ldia-gensim", "ldia-tsne7", "ldia-coherence", "ldia-tsne47"];
+var lsa_id = ["lsa-tsne7", "lsa-coherence", "lsa-tsne2"];
 
 function graphChanger(element) {
   // Check which graphs to change
   if (element.classList.contains("label-image")) {
-    graph_count = label_graph_count
-    graph_id = label_graph_id
+    count = label_count
+    id = label_id
   }
   else if (element.classList.contains("label-pie")) {
-    graph_count = pie_graph_count
-    graph_id = pie_graph_id
+    count = pie_count
+    id = pie_id
   }
   else if (element.classList.contains("desc-graphs")) {
-    graph_count = desc_graph_count
-    graph_id = desc_graph_id
+    count = desc_count
+    id = desc_id
   }
-  else {
-    graph_count = model_graph_count
-    graph_id = model_graph_id
+  else if (element.classList.contains("model-graphs")) {
+    count = model_count
+    id = model_id
+  }
+  else if (element.classList.contains("ldia-graphs")) {
+    count = ldia_count
+    id = ldia_id
+  }
+  else if (element.classList.contains("lsa-graphs")) {
+    count = lsa_count
+    id = lsa_id
   }
 
-  var current_display = document.getElementById(graph_id[graph_count]);
+  var current_display = document.getElementById(id[count]);
 
   // Update the counter
   if (element.classList.contains("next-button")) {
-    graph_count += 1;
+    count += 1;
   }
   else {
-    graph_count -= 1;
+    count -= 1;
   }
-  if (graph_count > graph_id.length - 1) {
-    graph_count = 0;
+  if (count > id.length - 1) {
+    count = 0;
   }
-  if (graph_count < 0) {
-    graph_count = graph_id.length - 1;
+  if (count < 0) {
+    count = id.length - 1;
   }
 
   // Update the global variable values
   if (element.classList.contains("label-image")) {
-    label_graph_count = graph_count
-    label_graph_id = graph_id
+    label_count = count
+    label_id = id
   }
   else if (element.classList.contains("label-pie")) {
-    pie_graph_count = graph_count
-    pie_graph_id = graph_id
+    pie_count = count
+    pie_id = id
   }
   else if (element.classList.contains("desc-graphs")) {
-    desc_graph_count = graph_count
-    desc_graph_id = graph_id
+    desc_count = count
+    desc_id = id
   }
-  else {
-    model_graph_count = graph_count
-    model_graph_id = graph_id
+  else if (element.classList.contains("model-graphs")) {
+    model_count = count
+    model_id = id
+  }
+  else if (element.classList.contains("ldia-graphs")) {
+    ldia_count = count
+    ldia_id = id
+  }
+  else if (element.classList.contains("lsa-graphs")) {
+    lsa_count = count
+    lsa_id = id
   }
 
   // Hide the current graph and display the next
   current_display.style.display = "none";
-  var next_display = document.getElementById(graph_id[graph_count]);
+  var next_display = document.getElementById(id[count]);
   next_display.style.display = "block";
 }
